0b80b596e2decd8b8f25fabdaefbc886e838864b
in15 <103326160+Andrejj23@users.noreply.github.com>
2022-06-29 09:43:18 +0200

    coomit3
    Porudzbina se kreira, medjutim ne radi metod za brisanje artikla iz porudzbine

--- demo-front/src/views/KupacKreiranjePorudzbineView.vue
+++ demo-front/src/views/KupacKreiranjePorudzbineView.vue
@@ -23,6 +23,7 @@
             <td>{{artikal.naziv }}</td>
             <td>{{artikal.cena}}</td>
             <td>{{artikal.kolicina}}</td>
+            <button v-on:click="brisanje_artikla(artikal)">Ponisti </button>
           </tr>
   
      </table>
@@ -30,7 +31,7 @@
     <p>Ukupna cena porudzbine iznosi: {{this.PregledPorudzbine.ukupnaCena}}<br>
     Cena sa popustom iznosi: {{this.PregledPorudzbine.cena_sa_popustom}}
     </p>
-
+    <button v-on:click="kreiraj_porudzbinu()">NARUCI</button>
     
 
     <!-- napravi neku kao korpu gde mogu da se dodaju artikli, i brisu i na kraju se napravi porudzbina kad se klikne ovo dugme ispod, kao korpa neka-->
@@ -47,6 +48,7 @@ export default {
   data: function(){
     return{
       PregledPorudzbine:"",
+      
     };
   },
   mounted: function () {
@@ -96,6 +98,46 @@ export default {
         });
 
       },
+
+      kreiraj_porudzbinu : function (){
+        fetch('http://localhost:8081/api/porudzbina/kreiranjePorudzbine', {
+        method: "GET",
+        credentials: 'include',
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        //body: JSON.stringify(this.logovanjeSlanje),
+
+      })
+        .then(response => response.json())
+        .then(data => {console.log("Success:", data);
+        this.$router.push("/kupacPorudzbine");
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          
+        });
+      },
+
+      brisanje_artikla: function(artikal) {
+        // vodi racuna ono dto brisanje tri odjednom, da se obrisu i lokacija, i restoran i menadzer zaduzen za taj restoran
+        console.log(artikal.id);
+        fetch("http://localhost:8081/api/porudzbina/obrisiArtikal/" + artikal.id, {
+        method: "DELETE",
+        credentials: 'include',
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+      }).then((res) => {
+        if (res.ok) {
+          window.location.reload();
+        }
+      }).catch((error) => {
+          console.error("Error:", error);
+        });
+    },
   }
 };
 
