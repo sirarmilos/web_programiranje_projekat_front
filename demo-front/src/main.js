438be75661e57c41d5e7ee1cc0b0b4f5f802d344
in15 <103326160+Andrejj23@users.noreply.github.com>
2022-07-01 21:36:16 +0200

    commit6
    Ubacen simbol korpe

--- demo-front/src/main.js
+++ demo-front/src/main.js
@@ -5,4 +5,11 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCartShopping)
+
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- demo-front/src/views/KupacPocetnaView.vue
+++ demo-front/src/views/KupacPocetnaView.vue
@@ -6,7 +6,7 @@
     <div class="topnav">
         <a href="/kupacRestorani">Restorani</a>
         <a href="/kupacPorudzbine">Porudžbina</a>
-        <a href="/kupackreiranjePorudzbine">Korpa</a>
+        <a href="/kupackreiranjePorudzbine"><font-awesome-icon icon="fa-solid fa-cart-shopping" /></a>
         <a class="active" href="/kupacPocetna" >Pregled podataka</a>
         <a href="/kupacAzuriranjePodataka">Ažuriranje podataka</a>
         <a v-on:click="odlogovanje()">Izloguj se</a>
